21428d9a7e4da27a7416536415583851a75df667
chanpi <teemoco@gmail.com>
2012-04-10 22:38:05 +0900

    Fixed

---
+++ HamSTART/HamSTART.cpp
@@ -0,0 +1,234 @@
+// HamSTART.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "HamSTART.h"
+
+#define MAX_LOADSTRING	100
+#define TIMER_ID		100
+
+// グローバル変数:
+HINSTANCE hInst;								// 現在のインターフェイス
+TCHAR szTitle[MAX_LOADSTRING];					// タイトル バーのテキスト
+TCHAR szWindowClass[MAX_LOADSTRING];			// メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+static int g_nTimerInterval = 5000;
+static TCHAR g_szTarget1[MAX_PATH] = {0};
+static TCHAR g_szTarget2[MAX_PATH] = {0};
+static LPCTSTR g_szHamster = _T("Hamster");
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: ここにコードを挿入してください。
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// グローバル文字列を初期化しています。
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_HAMSTART, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// アプリケーションの初期化を実行します:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_HAMSTART));
+
+	// メイン メッセージ ループ:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+//  コメント:
+//
+//    この関数および使い方は、'RegisterClassEx' 関数が追加された
+//    Windows 95 より前の Win32 システムと互換させる場合にのみ必要です。
+//    アプリケーションが、関連付けられた
+//    正しい形式の小さいアイコンを取得できるようにするには、
+//    この関数を呼び出してください。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_HAMSTART));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_HAMSTART);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します。
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int /*nCmdShow*/)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // グローバル変数にインスタンス処理を格納します。
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   //ShowWindow(hWnd, nCmdShow);
+   //UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:  メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND	- アプリケーション メニューの処理
+//  WM_PAINT	- メイン ウィンドウの描画
+//  WM_DESTROY	- 中止メッセージを表示して戻る
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	STARTUPINFO si = {0};
+	PROCESS_INFORMATION pi = {0};
+
+	switch (message)
+	{
+	case WM_CREATE:
+		// iniファイル読み込み
+		{
+			TCHAR szIniFile[MAX_PATH] = {0};
+			_stprintf_s(szIniFile, _countof(szIniFile), _T("%s.ini"), szTitle);
+
+			// タイマーインターバル
+			g_nTimerInterval = GetPrivateProfileInt(g_szHamster, _T("TIMER_INTERVAL"), g_nTimerInterval, szIniFile);
+
+			// 起動ファイル１
+			GetPrivateProfileString(g_szHamster, _T("TARGET1"), _T("Hamster.exe"), g_szTarget1, _countof(g_szTarget1), szIniFile);
+			OutputDebugString(g_szTarget1);
+			OutputDebugString(_T("\n"));
+
+			// 起動ファイル２
+			GetPrivateProfileString(g_szHamster, _T("TARGET2"), _T("HamsterGateway.exe"), g_szTarget2, _countof(g_szTarget2), szIniFile);
+			OutputDebugString(g_szTarget2);
+			OutputDebugString(_T("\n"));
+		
+			SetTimer(hWnd, TIMER_ID, g_nTimerInterval, NULL);
+		}
+		si.cb = sizeof(STARTUPINFO);
+		CreateProcess(NULL, g_szTarget1, NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
+		CloseHandle(pi.hThread);
+		CloseHandle(pi.hProcess);
+		break;
+
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 選択されたメニューの解析:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_TIMER:
+		si.cb = sizeof(STARTUPINFO);
+		CreateProcess(NULL, g_szTarget2, NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
+		CloseHandle(pi.hThread);
+		CloseHandle(pi.hProcess);
+		PostMessage(hWnd, WM_CLOSE, 0, 0);
+		break;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 描画コードをここに追加してください...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
